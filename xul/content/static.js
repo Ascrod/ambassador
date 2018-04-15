b34fb9b36dbc96706b0cf839742c246a85495cc0
Ascrod <32915892+Ascrod@users.noreply.github.com>
2018-04-15 10:47:48 -0400

    Update app version suffix.

--- xul/content/static.js
+++ xul/content/static.js
@@ -4,7 +4,7 @@
 
 const __cz_version   = "0.9.93";
 const __cz_condition = "green";
-const __cz_suffix    = "rdmsoft";
+const __cz_suffix    = "uxp";
 const __cz_guid      = "59c81df5-4b7a-477b-912d-4e0fdf64e5f2";
 const __cz_locale    = "0.9.92";
 
