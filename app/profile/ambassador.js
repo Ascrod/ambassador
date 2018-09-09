08c8f459f99c1921f69bbe47721629688a5c0f36
Ascrod <32915892+Ascrod@users.noreply.github.com>
2018-09-09 12:36:03 -0400

    Combine app and update prefs files.

--- app/profile/ambassador.js
+++ app/profile/ambassador.js
@@ -1,9 +1,11 @@
-// Default Prefs
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 // This is needed to start Ambassador.
 pref("toolkit.defaultChromeURI", "chrome://ambassador/content/chatzilla.xul");
 
-// Enable the extension manager...
+// Enable the extension manager.
 pref("xpinstall.dialog.confirm", "chrome://mozapps/content/xpinstall/xpinstallConfirm.xul");
 pref("xpinstall.dialog.progress.skin", "chrome://mozapps/content/extensions/extensions.xul?type=themes");
 pref("xpinstall.dialog.progress.chrome", "chrome://mozapps/content/extensions/extensions.xul?type=extensions");
@@ -27,10 +29,32 @@ pref("extensions.getMoreThemesURL", "chrome://mozapps/locale/extensions/extensio
 
 pref("extensions.getAddons.showPane", false);
 
-// make the external protocol service happy
+// Make the external protocol service happy
 pref("network.protocol-handler.expose-all", false);
 pref("network.protocol-handler.expose.irc", true);
 pref("network.protocol-handler.expose.ircs", true);
 pref("security.dialog_enable_delay", 0);
 
 pref("general.useragent.extra.ambassador", "Ambassador/@REVISION@");
+
+// Update prefs
+
+// Always prompt by default.
+pref("app.update.enabled", true);
+pref("app.update.auto", false);
+pref("app.update.silent", false);
+pref("app.update.mode", 0);
+pref("app.update.incompatible.mode", 0);
+
+pref("app.update.url", "http://chatzilla.rdmsoft.com/xulrunner/update/2/%CHANNEL%?v=%VERSION%&b=%BUILD_ID%&o=%BUILD_TARGET%&ov=%OS_VERSION%&pv=%PLATFORM_VERSION%&");
+pref("app.update.url.manual", "https://github.com/ascrod/ambassador/");
+pref("app.update.url.details", "https://github.com/ascrod/ambassador/");
+
+// Check every day, if download or install is deferred ask again each day.
+pref("app.update.interval", 86400);
+pref("app.update.nagTimer.download", 86400);
+pref("app.update.nagTimer.restart", 86400);
+pref("app.update.timer", 600000);
+
+// Seems to be broken, and nothing else uses it.
+pref("app.update.showInstalledUI", false);
