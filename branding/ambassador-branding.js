5b7811a245a44284ba22506510dd8e050ad5806a
Ascrod <32915892+Ascrod@users.noreply.github.com>
2019-01-20 08:05:50 -0500

    Fix devtools prefs.

--- branding/ambassador-branding.js
+++ branding/ambassador-branding.js
@@ -9,3 +9,7 @@ pref("app.releaseNotesURL", "https://www.github.com/ascrod/ambassador/releases/l
 // Updates disabled
 pref("app.update.enabled", false);
 pref("app.update.url", "");
+
+// Number of usages of the web console or scratchpad.
+// If this is less than 5, then pasting code into the web console or scratchpad is disabled
+pref("devtools.selfxss.count", 100);
