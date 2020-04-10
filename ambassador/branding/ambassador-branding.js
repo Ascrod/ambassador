0c53c97f31307916279eda7fad42661720cc84d8
Ascrod <32915892+Ascrod@users.noreply.github.com>
2020-04-10 16:15:51 -0400

    Fix issues with branding prefs file.
    Tag#69.

--- ambassador/branding/ambassador-branding.js
+++ ambassador/branding/ambassador-branding.js
@@ -1,7 +1,5 @@
 #filter substitution
 #filter emptyLines
-#include ../../shared/pref/preferences.inc
-#include ../../shared/pref/uaoverrides.inc
 
 pref("startup.homepage_override_url","https://www.github.com/ascrod/ambassador/");
 pref("app.releaseNotesURL", "https://www.github.com/ascrod/ambassador/releases/latest/");
